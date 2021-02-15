80d0ef3d7baa9d4333bd2b940a43793b7337aac0
Guestin Jason <jason_guestin@ens.univ-artois.fr>
2021-02-15 20:54:16 +0100

    Création d'une base d'API avc authentification JWT

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const db = require("./app/models");
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+db.sequelize.sync().then(() => {
+  console.log('DB is synchronized');
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to AquariumWatcher application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
